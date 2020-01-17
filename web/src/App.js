988087e85fe8a1ad9989c7fdfe314cb22bdc2539
Dannylo Johnathan <dannylojohnathan@gmail.com>
2020-01-17 18:31:26 -0300

    tela inicial, cadastro de usuários e disposição dos usuários cadastrados.

---
+++ web/src/App.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+
+function App() {
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatitude(latitude);
+        setLongitude(longitude);
+
+      }, 
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []); 
+
+  async function handleAddDev(e){
+    e.preventDefault();
+
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required 
+              value ={github_username}
+              onChange = {e => setGithubUsername(e.target.value)}/>
+         </div>
+
+         <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required 
+              value ={techs}
+              onChange = {e => setTechs(e.target.value)}/>
+         </div>
+
+        <div className="input-group">
+          <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="number" name="latitude" id="latitude" onChange={e => setLatitude(e.target.value)} required value ={latitude}/>
+          </div>
+          <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="number" name="longitude" id="longitude" required value ={longitude} onChange={e => setLongitude(e.target.value)} />
+          </div>
+         </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+              <header>
+                  <img src="https://avatars0.githubusercontent.com/u/1091183?s=460&v=4" alt="Dannylo Johnathan"/>
+                  <div className="user-info">
+                    <strong>Dannylo Johnathan</strong>
+                    <span>Java 8, Spring Boot</span>
+                  </div>
+              </header>
+              <p>Researcher (UFRN-Natal) of Software Engineering focusing on the Internet of Things, Mobile Computing and Ubiquitous Computing. Java Dev.</p>
+              <a href="https://github.com/dannylo">Acessar perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+              <header>
+                  <img src="https://avatars0.githubusercontent.com/u/1091183?s=460&v=4" alt="Dannylo Johnathan"/>
+                  <div className="user-info">
+                    <strong>Dannylo Johnathan</strong>
+                    <span>Java 8, Spring Boot</span>
+                  </div>
+              </header>
+              <p>Researcher (UFRN-Natal) of Software Engineering focusing on the Internet of Things, Mobile Computing and Ubiquitous Computing. Java Dev.</p>
+              <a href="https://github.com/dannylo">Acessar perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+              <header>
+                  <img src="https://avatars0.githubusercontent.com/u/1091183?s=460&v=4" alt="Dannylo Johnathan"/>
+                  <div className="user-info">
+                    <strong>Dannylo Johnathan</strong>
+                    <span>Java 8, Spring Boot</span>
+                  </div>
+              </header>
+              <p>Researcher (UFRN-Natal) of Software Engineering focusing on the Internet of Things, Mobile Computing and Ubiquitous Computing. Java Dev.</p>
+              <a href="https://github.com/dannylo">Acessar perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+              <header>
+                  <img src="https://avatars0.githubusercontent.com/u/1091183?s=460&v=4" alt="Dannylo Johnathan"/>
+                  <div className="user-info">
+                    <strong>Dannylo Johnathan</strong>
+                    <span>Java 8, Spring Boot</span>
+                  </div>
+              </header>
+              <p>Researcher (UFRN-Natal) of Software Engineering focusing on the Internet of Things, Mobile Computing and Ubiquitous Computing. Java Dev.</p>
+              <a href="https://github.com/dannylo">Acessar perfil do GitHub</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
